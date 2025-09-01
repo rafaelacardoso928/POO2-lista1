0e28bde4f4fc3192da719dd40ad862c40ce443dd
rafaelacardoso928 <cardosorafaela.2802@gmail.com>
2025-09-01 17:29:22 -0300

    Add files via upload

---
+++ MainConcatenador/src/mainconcatenador/Concatenador.java
@@ -0,0 +1,14 @@
+
+package mainconcatenador;
+
+    public class Concatenador {
+    public void concatenar() {
+        String parte1 = "Olá,";
+        String parte2 = " mundo!";
+        String resultado = parte1 + parte2;
+
+        System.out.println(resultado);
+    }
+}
+
+
---
+++ MainConcatenador/src/mainconcatenador/MainConcatenador.java
@@ -0,0 +1,13 @@
+
+package mainconcatenador;
+
+     public class MainConcatenador {
+    public static void main(String[] args) {
+        Concatenador concat = new Concatenador(); 
+        concat.concatenar();                      
+    }
+}
+
+    
+    
+
